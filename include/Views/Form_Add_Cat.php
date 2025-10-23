695b71f0e86bb55e98e3e07344f8a665f05a4e5e
Manuel Feria <manuel.jesus.feria.santana@ieslaarboleda.es>
2025-10-24 01:33:05 +0200

    Validacion_BackEnd

--- include/Views/Form_Add_Cat.php
+++ include/Views/Form_Add_Cat.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Popup</title>
-    <link rel="stylesheet" href="../assets/css/principalw.css">
+    <link rel="stylesheet" href="../assets/css/principal.css">
 </head>
 <body>
     <!-------------------------------LOGIC------------------------------->
